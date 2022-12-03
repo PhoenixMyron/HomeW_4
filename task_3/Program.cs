bada84d27dfc52fe2ecdb2866e6bf12f28241bb5
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-04 01:38:53 +0600

    added task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите количество элементов массива:\t");
+int elementsCount = int.Parse(Console.ReadLine());
+
+int[] arr = new int[elementsCount];
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine("Вывод массива:");
+    Console.WriteLine($"{string.Join(", ", arr)}");
+
+}
+
+
+PrintArray(arr);
