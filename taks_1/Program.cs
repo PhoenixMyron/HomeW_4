1fec38f1f74a398932b085e453d081a723659d0c
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-04 00:54:40 +0600

    added task 1

---
+++ taks_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// НЕ ИСПОЛЬЗОВАТЬ MATH.POW
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n2 = Convert.ToInt32(Console.ReadLine()), result = n1, z = 0;
+for ( int count = 2; count <= n2; count++ )
+{
+result *= n1;
+z = result;
+}
+Console.Write($"{z} ");
+
